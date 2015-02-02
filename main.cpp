127386083fc1f01c54e6d40ade9bf7139f629628
moepwizzy <marcus.boden@posteo.de>
2015-02-02 14:05:33 +0100

    improved threading

--- main.cpp
+++ main.cpp
@@ -15,28 +15,45 @@
 #include "../threadpool/threadpool.h"
 
 struct train_struct {
-  libsvm *problem;
+  class_problem *cp;
   std::vector<double> *results;
   int problem_number;
-  train_struct(libsvm *p, std::vector<double> *r, int n) 
-    : problem(p), results(r), problem_number(n) {}; 
+  train_struct(class_problem *p, std::vector<double> *r, int n) 
+    : cp(p), results(r), problem_number(n) {}; 
 };
 
 void thread_train (void * prob_ptr) {
   train_struct *tr_str = (train_struct *) prob_ptr;
-  libsvm *problem = tr_str->problem;
-  if(!problem->prepare()) {
+  libsvm problem;
+  std::vector<profile*> data_points = tr_str->cp->get_positive_training();
+  for (std::vector<profile*>::iterator it = data_points.begin();
+      it != data_points.end(); ++it)
+    problem.addTrainSeq((*it)->get_representing_vector(),POS);
+  data_points = tr_str->cp->get_negative_training();
+  for (std::vector<profile*>::iterator it = data_points.begin();
+      it != data_points.end(); ++it)
+    problem.addTrainSeq((*it)->get_representing_vector(),NEG);
+  data_points = tr_str->cp->get_positive_testing();
+  for (std::vector<profile*>::iterator it = data_points.begin();
+      it != data_points.end(); ++it)
+    problem.addTestSeq((*it)->get_representing_vector(),POS);
+  data_points = tr_str->cp->get_negative_testing();
+  for (std::vector<profile*>::iterator it = data_points.begin();
+      it != data_points.end(); ++it)
+    problem.addTestSeq((*it)->get_representing_vector(),NEG);
+  delete tr_str->cp;
+  if(!problem.prepare()) {
       print("prepare failed");
       exit(1);
   }
-  if(!problem->train()) {
+  if(!problem.train()) {
       print("train failed");
       exit(1);
   }
-  double result = problem->evaluate();
-  (*tr_str->results)[tr_str->problem_number] = result < 0.5 ? 1 - result : result; 
-  print((*tr_str->results)[tr_str->problem_number]);
-  delete problem;
+  double result = problem.evaluate();
+  (*tr_str->results)[tr_str->problem_number] = result; //< 0.5 ? 1 - result : result; 
+  print(result);
+  delete tr_str;
 }
 
 int main(int argc, char** argv) {
@@ -48,26 +65,9 @@ int main(int argc, char** argv) {
   main_container.count_all_profiles(cl_args.number_of_threads);
   threadpool pool (cl_args.number_of_threads);
   std::vector<double> results (cp_vector.size(),-1);
-  for(unsigned int i = 0; i < cp_vector.size(); ++i) {
-    libsvm *problem = new libsvm;
-    std::vector<profile*> data_points = cp_vector[i]->get_positive_training();
-    for (std::vector<profile*>::iterator it = data_points.begin();
-        it != data_points.end(); ++it)
-      problem->addTrainSeq((*it)->get_representing_vector(),POS);
-    data_points = cp_vector[i]->get_negative_training();
-    for (std::vector<profile*>::iterator it = data_points.begin();
-        it != data_points.end(); ++it)
-      problem->addTrainSeq((*it)->get_representing_vector(),NEG);
-    data_points = cp_vector[i]->get_positive_testing();
-    for (std::vector<profile*>::iterator it = data_points.begin();
-        it != data_points.end(); ++it)
-      problem->addTestSeq((*it)->get_representing_vector(),POS);
-    data_points = cp_vector[i]->get_negative_testing();
-    for (std::vector<profile*>::iterator it = data_points.begin();
-        it != data_points.end(); ++it)
-      problem->addTestSeq((*it)->get_representing_vector(),NEG);
+  for(unsigned int i = 0; i < 3; ++i) {
     pool.addThread(thread_train, 
-        (void *) new train_struct(problem, &results, i));
+        (void *) new train_struct(cp_vector[i], &results, i));
   }
   pool.wait();
   double avg_roc = 0;
